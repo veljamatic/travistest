6372c6aa715c9ce7892a6c8bb658eadaab7dbcad
Velja Matic <veljamatic@gmail.com>
2016-10-30 13:56:59 +0100

    First unit tests

---
+++ src/Main.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Veljamatic\Travistest;
+
+use Illuminate\Container\Container;
+
+class Main
+{
+  /**
+  * Illuminate container instance.
+  *
+  * @var \Illuminate\Container\Container
+  */
+  protected $container;
+
+  /**
+   * Version for the request.
+   *
+   * @var string
+   */
+  protected $version;
+
+  /**
+   * Default version.
+   *
+   * @var string
+   */
+  protected $defaultVersion;
+
+  /**
+   * Create a new Main instance.
+   *
+   * @param \Illuminate\Container\Container   $container
+   *
+   * @return void
+   */
+  public function __construct(Container $container)
+  {
+    $this->container = $container;
+  }
+
+  /**
+   * Set the version of the API for the next request.
+   *
+   * @param string $version
+   *
+   * @return \Dingo\Api\Dispatcher
+   */
+  public function version($version)
+  {
+      $this->version = $version;
+
+      return $this;
+  }
+
+  /**
+   * Set the default version.
+   *
+   * @param string $version
+   *
+   * @return void
+   */
+  public function setDefaultVersion($version)
+  {
+      $this->defaultVersion = $version;
+  }
+
+  /**
+   * Get the version.
+   *
+   * @return string
+   */
+  public function getVersion()
+  {
+      return $this->version ?: $this->defaultVersion;
+  }
+}
---
+++ tests/MainTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Veljamatic\Travistest\Tests;
+
+use Mockery as m;
+use PHPUnit_Framework_TestCase;
+use Illuminate\Container\Container;
+use Veljamatic\Travistest\Main;
+
+class MainTest extends PHPUnit_Framework_TestCase
+{
+  public function setUp()
+  {
+    $this->container = new Container;
+    $this->main = new Main($this->container);
+
+    $this->main->setDefaultVersion('v1');
+  }
+
+  public function tearDown()
+  {
+      m::close();
+  }
+
+  public function testDefaultVersion()
+  {
+      $this->assertSame('v1', $this->main->getVersion());
+  }
+
+  public function testWithVersion()
+  {
+      $this->assertSame('v2', $this->main->version('v2')->getVersion());
+  }
+}
