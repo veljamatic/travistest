c49596c4009a9407730e6c7f9b1166e33db76cae
Velja Matic <veljamatic@gmail.com>
2016-10-30 14:07:59 +0100

    spacing fixed

--- src/Main.php
+++ src/Main.php
@@ -7,71 +7,71 @@ use Illuminate\Container\Container;
 class Main
 {
     /**
-   * Illuminate container instance.
-   *
-   * @var \Illuminate\Container\Container
-   */
-  protected $container;
+     * Illuminate container instance.
+     *
+     * @var \Illuminate\Container\Container
+     */
+    protected $container;
 
-  /**
-   * Version for the request.
-   *
-   * @var string
-   */
-  protected $version;
+    /**
+     * Version for the request.
+     *
+     * @var string
+     */
+    protected $version;
 
-  /**
-   * Default version.
-   *
-   * @var string
-   */
-  protected $defaultVersion;
+    /**
+     * Default version.
+     *
+     * @var string
+     */
+    protected $defaultVersion;
 
-  /**
-   * Create a new Main instance.
-   *
-   * @param \Illuminate\Container\Container   $container
-   *
-   * @return void
-   */
-  public function __construct(Container $container)
-  {
-      $this->container = $container;
-  }
+    /**
+     * Create a new Main instance.
+     *
+     * @param \Illuminate\Container\Container   $container
+     *
+     * @return void
+     */
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
 
-  /**
-   * Set the version of the API for the next request.
-   *
-   * @param string $version
-   *
-   * @return \Dingo\Api\Dispatcher
-   */
-  public function version($version)
-  {
-      $this->version = $version;
+    /**
+     * Set the version of the API for the next request.
+     *
+     * @param string $version
+     *
+     * @return \Dingo\Api\Dispatcher
+     */
+    public function version($version)
+    {
+        $this->version = $version;
 
-      return $this;
-  }
+        return $this;
+    }
 
-  /**
-   * Set the default version.
-   *
-   * @param string $version
-   *
-   * @return void
-   */
-  public function setDefaultVersion($version)
-  {
-      $this->defaultVersion = $version;
-  }
+    /**
+     * Set the default version.
+     *
+     * @param string $version
+     *
+     * @return void
+     */
+    public function setDefaultVersion($version)
+    {
+        $this->defaultVersion = $version;
+    }
 
-  /**
-   * Get the version.
-   *
-   * @return string
-   */
-  public function getVersion()
-  {
-      return $this->version ?: $this->defaultVersion;
-  }
+    /**
+     * Get the version.
+     *
+     * @return string
+     */
+    public function getVersion()
+    {
+        return $this->version ?: $this->defaultVersion;
+    }
 }
